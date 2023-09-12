abf39329efa0a016fc6b0f0c6718ea696be4d278
suap-dev <suap.dev@gmail.com>
2023-09-13 01:41:28 +0200

    Target dummy that flashes on hit.

---
+++ Source/ActionRoguelike/Private/STargetDummy.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "STargetDummy.h"
+
+// Sets default values
+ASTargetDummy::ASTargetDummy()
+{
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>("MeshComp");
+	RootComponent = MeshComp;
+
+	AttributeComp = CreateDefaultSubobject<USAttributeComponent>("AttributeComp");
+	// Trigger when health is changed (damage/healing)
+	AttributeComp->OnHealthChanged.AddDynamic(this, &ASTargetDummy::OnHealthChanged);
+}
+
+// ReSharper disable once CppMemberFunctionMayBeConst
+void ASTargetDummy::OnHealthChanged(AActor* InstigatorActor, USAttributeComponent* OwningComp, float NewHealth,
+                                    const float Delta)
+{
+	// We don't check for null, because we created it in the constructor. It's not a function parameter.
+	if (Delta < 0.0f)
+	{
+		MeshComp->SetScalarParameterValueOnMaterials("TimeToHit", GetWorld()->TimeSeconds);
+	}
+}
